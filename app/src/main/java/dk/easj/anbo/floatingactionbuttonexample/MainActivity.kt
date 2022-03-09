dccf9ab0e4babff7806828dc700aea797442db4d
Anders Børjesson <anbo@zealand.dk>
2022-03-09 13:27:41 +0100

    gradle update

--- app/src/main/java/dk/easj/anbo/floatingactionbuttonexample/MainActivity.kt
+++ app/src/main/java/dk/easj/anbo/floatingactionbuttonexample/MainActivity.kt
@@ -1,14 +1,13 @@
 package dk.easj.anbo.floatingactionbuttonexample
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import dk.easj.anbo.floatingactionbuttonexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
